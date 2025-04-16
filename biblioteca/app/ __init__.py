c06fc968de1440bb1db37f37aa4c5f310f1321a5
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2025-04-16 12:57:17 -0300

    Actualización 15/4/2025

---
+++ biblioteca/app/ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.database import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///biblioteca.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
+    
+    return app
